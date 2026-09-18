3521bf4b775afcab2aab936a6c432e9b3e9ac91d
cauaafonso241-creator <cauaafonso241@gmail.com>
2026-09-18 16:30:57 -0300

    Add cohesive illustrated face traits for all 15 fighters

--- src/data.js
+++ src/data.js
@@ -1,21 +1,21 @@
 export const P="assets/portraits/vereadores/";
 
 export const fighters=[
-  {name:"Afonsinho",abbr:"AF",portrait:P+"afonsinho.png",color:"#355d9a",power:"Força Incomparável",type:"power",specialB:"Avanço de Força",super:"Força Total",stats:[92,68,84]},
-  {name:"Donizete",abbr:"DZ",portrait:P+"donizete.png",color:"#6a4a43",power:"Fala Supersônica",type:"sonic",specialB:"Eco Rápido",super:"Onda Supersônica",stats:[76,88,74]},
-  {name:"Alan Matias",abbr:"AM",portrait:P+"alan-matias.png",color:"#2f5f82",power:"Super Chute",type:"kick",specialB:"Chute Ascendente",super:"Sequência de Chutes",stats:[86,88,72]},
-  {name:"Batoré",abbr:"BT",portrait:P+"batore.png",color:"#704438",power:"Bola de Futebol",type:"ball",specialB:"Bicicleta de Impacto",super:"Chuva de Bolas",stats:[82,78,78]},
-  {name:"Kleber do Cavaco",abbr:"KC",portrait:P+"kleber-do-cavaco.png",color:"#694f83",power:"Cavaco",type:"cavaco",specialB:"Acorde Rápido",super:"Solo do Cavaco",stats:[76,84,80]},
-  {name:"Dr. Anderson Veterinário",abbr:"AV",portrait:P+"dr-anderson-veterinario.png",color:"#586754",power:"Cães de Guarda",type:"dogs",specialB:"Investida Canina",super:"Matilha de Guarda",stats:[84,72,86]},
-  {name:"Edson Mota",abbr:"EM",portrait:P+"edson-mota.png",color:"#446b86",power:"Kickboxing Afiado",type:"kickbox",specialB:"Joelhada Rápida",super:"Combo Kickboxing",stats:[88,86,74]},
-  {name:"Guilherme do Salão",abbr:"GS",portrait:P+"guilherme-do-salao.png",color:"#604a78",power:"Tesourada",type:"scissors",specialB:"Corte Cruzado",super:"Tesoura Relâmpago",stats:[80,88,72]},
-  {name:"Jair do Bar",abbr:"JB",portrait:P+"jair-do-bar.png",color:"#7b6748",power:"Galão d'Água",type:"water",specialB:"Jato d'Água",super:"Maré de Galões",stats:[82,70,84]},
-  {name:"Marcinho",abbr:"MC",portrait:P+"marcinho.png",color:"#3f6885",power:"Processo Administrativo",type:"process",specialB:"Despacho Rápido",super:"Pilha de Processos",stats:[76,84,80]},
-  {name:"Ronaldo da Comissão",abbr:"RC",portrait:P+"ronaldo-da-comissao.png",color:"#5d4a78",power:"Peso Pesado",type:"dumbbell",specialB:"Levantamento Rápido",super:"Carga Máxima",stats:[92,66,86]},
-  {name:"Rony",abbr:"RN",portrait:P+"rony.png",color:"#365f80",power:"Galão d'Água",type:"water",specialB:"Jato d'Água",super:"Maré de Galões",stats:[82,82,78]},
-  {name:"Tinho",abbr:"TH",portrait:P+"tinho.png",color:"#45644e",power:"Super Chute",type:"kick",specialB:"Chute Ascendente",super:"Sequência de Chutes",stats:[88,74,82]},
-  {name:"Topete",abbr:"TP",portrait:P+"topete.png",color:"#6c5a37",power:"Giro Descontrolado",type:"spin",specialB:"Arranque Giratório",super:"Turbilhão",stats:[80,94,70]},
-  {name:"Xuxa",abbr:"XX",portrait:P+"xuxa.png",color:"#6c523d",power:"Chicote de Cabelo",type:"hair",specialB:"Corte Aéreo",super:"Redemoinho de Cabelo",stats:[78,90,72]}
+  {name:"Afonsinho",abbr:"AF",portrait:P+"afonsinho.png",color:"#355d9a",power:"Força Incomparável",type:"power",specialB:"Avanço de Força",super:"Força Total",stats:[92,68,84,look:{skin:"#c99573",hair:"#d5d0c7",beard:"#c7c1b7",hairStyle:"bald",beardStyle:"full",glasses:true}},
+  {name:"Donizete",abbr:"DZ",portrait:P+"donizete.png",color:"#6a4a43",power:"Fala Supersônica",type:"sonic",specialB:"Eco Rápido",super:"Onda Supersônica",stats:[76,88,74,look:{skin:"#c58e6e",hair:"#3b3735",beard:"#62554f",hairStyle:"swept",beardStyle:"stubble",glasses:false}},
+  {name:"Alan Matias",abbr:"AM",portrait:P+"alan-matias.png",color:"#2f5f82",power:"Super Chute",type:"kick",specialB:"Chute Ascendente",super:"Sequência de Chutes",stats:[86,88,72,look:{skin:"#aa6d4d",hair:"#121c24",beard:"#29221f",hairStyle:"short",beardStyle:"none",glasses:false}},
+  {name:"Batoré",abbr:"BT",portrait:P+"batore.png",color:"#704438",power:"Bola de Futebol",type:"ball",specialB:"Bicicleta de Impacto",super:"Chuva de Bolas",stats:[82,78,78,look:{skin:"#b67654",hair:"#172029",beard:"#26201d",hairStyle:"slick",beardStyle:"none",glasses:false}},
+  {name:"Kleber do Cavaco",abbr:"KC",portrait:P+"kleber-do-cavaco.png",color:"#694f83",power:"Cavaco",type:"cavaco",specialB:"Acorde Rápido",super:"Solo do Cavaco",stats:[76,84,80,look:{skin:"#ae6d50",hair:"#24201e",beard:"#302a27",hairStyle:"bald",beardStyle:"goatee",glasses:false}},
+  {name:"Dr. Anderson Veterinário",abbr:"AV",portrait:P+"dr-anderson-veterinario.png",color:"#586754",power:"Cães de Guarda",type:"dogs",specialB:"Investida Canina",super:"Matilha de Guarda",stats:[84,72,86,look:{skin:"#9f6c53",hair:"#44423e",beard:"#3f403d",hairStyle:"bald",beardStyle:"full",glasses:false}},
+  {name:"Edson Mota",abbr:"EM",portrait:P+"edson-mota.png",color:"#446b86",power:"Kickboxing Afiado",type:"kickbox",specialB:"Joelhada Rápida",super:"Combo Kickboxing",stats:[88,86,74,look:{skin:"#b67856",hair:"#111b22",beard:"#211e1c",hairStyle:"short",beardStyle:"none",glasses:false}},
+  {name:"Guilherme do Salão",abbr:"GS",portrait:P+"guilherme-do-salao.png",color:"#604a78",power:"Tesourada",type:"scissors",specialB:"Corte Cruzado",super:"Tesoura Relâmpago",stats:[80,88,72,look:{skin:"#83533f",hair:"#111a20",beard:"#171817",hairStyle:"short",beardStyle:"full",glasses:false}},
+  {name:"Jair do Bar",abbr:"JB",portrait:P+"jair-do-bar.png",color:"#7b6748",power:"Galão d'Água",type:"water",specialB:"Jato d'Água",super:"Maré de Galões",stats:[82,70,84,look:{skin:"#9d725d",hair:"#d2d2cb",beard:"#c6c3bd",hairStyle:"short",beardStyle:"none",glasses:true}},
+  {name:"Marcinho",abbr:"MC",portrait:P+"marcinho.png",color:"#3f6885",power:"Processo Administrativo",type:"process",specialB:"Despacho Rápido",super:"Pilha de Processos",stats:[76,84,80,look:{skin:"#a86e53",hair:"#263139",beard:"#342c28",hairStyle:"fade",beardStyle:"none",glasses:true}},
+  {name:"Ronaldo da Comissão",abbr:"RC",portrait:P+"ronaldo-da-comissao.png",color:"#5d4a78",power:"Peso Pesado",type:"dumbbell",specialB:"Levantamento Rápido",super:"Carga Máxima",stats:[92,66,86,look:{skin:"#785444",hair:"#3d4242",beard:"#444541",hairStyle:"short",beardStyle:"full",glasses:false}},
+  {name:"Rony",abbr:"RN",portrait:P+"rony.png",color:"#365f80",power:"Galão d'Água",type:"water",specialB:"Jato d'Água",super:"Maré de Galões",stats:[82,82,78,look:{skin:"#724936",hair:"#10191f",beard:"#191817",hairStyle:"short",beardStyle:"none",glasses:false}},
+  {name:"Tinho",abbr:"TH",portrait:P+"tinho.png",color:"#45644e",power:"Super Chute",type:"kick",specialB:"Chute Ascendente",super:"Sequência de Chutes",stats:[88,74,82,look:{skin:"#93634f",hair:"#b7b6af",beard:"#77736c",hairStyle:"bald",beardStyle:"goatee",glasses:true}},
+  {name:"Topete",abbr:"TP",portrait:P+"topete.png",color:"#6c5a37",power:"Giro Descontrolado",type:"spin",specialB:"Arranque Giratório",super:"Turbilhão",stats:[80,94,70,look:{skin:"#714a39",hair:"#101920",beard:"#17191a",hairStyle:"short",beardStyle:"short",glasses:true}},
+  {name:"Xuxa",abbr:"XX",portrait:P+"xuxa.png",color:"#6c523d",power:"Chicote de Cabelo",type:"hair",specialB:"Corte Aéreo",super:"Redemoinho de Cabelo",stats:[78,90,72,look:{skin:"#b97859",hair:"#d2ad68",beard:"#67483a",hairStyle:"swept",beardStyle:"full",glasses:false}}
 ];
 
 export const maps=[
